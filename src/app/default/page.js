4e55f510f707ac59aaf07697944413162ed1189d
SanthiagoCL <sanrhiagolima@gmail.com>
2025-08-25 20:24:39 -0300

    Aula dia 25/08

---
+++ src/app/default/page.js
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Default() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-gray-900">Bem-vindo à Página Padrão!</h1>
+        </div>
+    );
+}
